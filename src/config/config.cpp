1c6f3dde3bf7183eb379781c13ea9a633881b04c
kichnu <kontakt@krzysztoforlinski.pl>
2025-09-06 15:34:31 +0200

    update credentilas

--- src/config/config.cpp
+++ src/config/config.cpp
@@ -14,7 +14,9 @@ bool pumpGlobalEnabled = true;  // Default ON
 unsigned long pumpDisabledTime = 0;
 const unsigned long PUMP_AUTO_ENABLE_MS = 3 * 60 * 1000; // 30 minutes
 
+// const char* ADMIN_PASSWORD_HASH = "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8";
 const char* ADMIN_PASSWORD_HASH = "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8";
+
 
 const IPAddress ALLOWED_IPS[] = {
     IPAddress(192, 168, 0, 124),
@@ -24,8 +26,12 @@ const IPAddress ALLOWED_IPS[] = {
 };
 const int ALLOWED_IPS_COUNT = sizeof(ALLOWED_IPS) / sizeof(ALLOWED_IPS[0]);
 
-const char* VPS_URL = "http://195.117.36.43:5000/api/water-events";
-const char* VPS_AUTH_TOKEN = "sha256:7b4f8a9c2e6d5a1b8f7e4c9a6d3b2f8e5c1a7b4f9e6d3c8a5b2f7e4c9a6d1b8f";;
+// const char* VPS_URL = "http://195.117.36.43:5000/api/water-events";
+const char* VPS_URL = "https://app.krzysztoforlinski.pl/api/water-events";
+
+// const char* VPS_AUTH_TOKEN = "sha256:7b4f8a9c2e6d5a1b8f7e4c9a6d3b2f8e5c1a7b4f9e6d3c8a5b2f7e4c9a6d1b8f";;
+const char* VPS_AUTH_TOKEN = "212d4a3d708f907c2c782937f72daf28aa66d8e342d4ede15381b6d8295344d6";;
+
 // const char* VPS_AUTH_TOKEN = "WaterSystem2024_StaticToken_ESP32C3";
 
 // const char* DEVICE_ID = "ESP32C3_WaterPump_001";
